3f05ff8f69f0d6203a11c1172e2f2e8d6684625d
Dylan Currier <dylan@dylansweb.com>
2021-10-27 04:39:24 -0400

    Initializing project directory, adding first file.

---
+++ RDS.Projects/RDS.TextParser/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RDS.TextParser
+{
+    public static class Constants
+    {
+        public static readonly Type t_string = typeof(string);
+        public static readonly Type t_bool = typeof(bool);
+        public static readonly Type t_byte = typeof(byte);
+        public static readonly Type t_sbyte = typeof(sbyte);
+        public static readonly Type t_char = typeof(char);
+        public static readonly Type t_decimal = typeof(decimal);
+        public static readonly Type t_double = typeof(double);
+        public static readonly Type t_float = typeof(float);
+        public static readonly Type t_int = typeof(int);
+        public static readonly Type t_uint = typeof(uint);
+        public static readonly Type t_long = typeof(long);
+        public static readonly Type t_ulong = typeof(ulong);
+        public static readonly Type t_object = typeof(object);
+        public static readonly Type t_short = typeof(short);
+        public static readonly Type t_ushort = typeof(ushort);
+        public static readonly Type t_DateTime = typeof(DateTime);
+    }
+}
